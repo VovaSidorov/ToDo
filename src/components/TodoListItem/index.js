b3b3eaebad73637b9383c36c5bfbe01818e4e553
VovaSidorov <VovaCivic@gmail.com>
2019-10-27 22:13:20 +0200

    Error: Invalid hook call

--- src/components/TodoListItem/index.js
+++ src/components/TodoListItem/index.js
@@ -5,9 +5,58 @@ import DoneIcon from '@material-ui/icons/Done';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
 
-const TodoListItem = ({label,important = false})=>{
+// const TodoListItem = ({label,important = false})=>{
+//
+//     const useStyles = makeStyles(theme => ({
+//
+//         root: {
+//             display: 'flex',
+//             flexDirection: 'row',
+//             marginTop: '5px',
+//             width:'100%',
+//             justifyContent: 'space-between'
+//         },
+//         item: {
+//             display:'flex',
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//             padding: theme.spacing(2, 1),
+//         },
+//     }));
+//
+//     const style = {
+//         color: important?'tomato':'black',
+//     };
+//     const classes = useStyles();
+//
+//     return (
+//         <Paper className={classes.root}>
+//             <div className={classes.item}>
+//                 <span   style={style}>{label}</span>
+//             </div>
+//
+//             <div >
+//                 <IconButton className={classes.button} aria-label="delete">
+//                     <DoneIcon />
+//                 </IconButton>
+//                 <IconButton aria-label="delete">
+//                     <DeleteIcon />
+//                 </IconButton>
+//             </div>
+//         </Paper>
+//
+//     );
+// };
+//
+// export default TodoListItem;
 
-    const useStyles = makeStyles(theme => ({
+export default class TodoListItem extends Component {
+
+    render (){
+
+        const {label, important = false}=this.props;
+
+        const useStyles = makeStyles(theme => ({
 
         root: {
             display: 'flex',
@@ -46,10 +95,8 @@ const TodoListItem = ({label,important = false})=>{
         </Paper>
 
     );
-};
-
-export default TodoListItem;
-
+    };
+    }
 
 
 
